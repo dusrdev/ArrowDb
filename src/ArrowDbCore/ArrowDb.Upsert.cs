142bd60687faaa13c70f94f8656b8d5d8abc219d
David Shnayder <dusrdev@gmail.com>
2025-02-01 16:26:57 +0200

    Optimized execution path of Upsert

--- src/ArrowDbCore/ArrowDb.Upsert.cs
+++ src/ArrowDbCore/ArrowDb.Upsert.cs
@@ -5,6 +5,22 @@ namespace ArrowDbCore;
 
 public partial class ArrowDb {
 	/// <summary>
+	/// Upsert the specified key with the specified value into the database
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
+	/// <param name="key">The key at which to upsert the value</param>
+	/// <param name="value">The value to upsert</param>
+	/// <param name="jsonTypeInfo">The json type info for the value type</param>
+	/// <returns>True</returns>
+	public bool Upsert<TValue>(ReadOnlySpan<char> key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo) {
+		WaitIfSerializing(); // block if the database is currently serializing
+		byte[] utf8value = JsonSerializer.SerializeToUtf8Bytes(value, jsonTypeInfo);
+		Lookup[key] = utf8value;
+		OnChangeInternal(ArrowDbChangeEventArgs.Upsert); // trigger change event
+		return true;
+	}
+
+	/// <summary>
 	/// Tries to upsert the specified key with the specified value into the database
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
@@ -21,16 +37,11 @@ public partial class ArrowDb {
 	/// <para>2. A value for the specified key exists and successfully deserialized to <typeparamref name="TValue"/></para>
 	/// <para>3. <paramref name="updateCondition"/> on the reference value returns false</para>
 	/// </remarks>
-	public bool Upsert<TValue>(ReadOnlySpan<char> key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo, Func<TValue, bool>? updateCondition = null) {
-		if (updateCondition is not null &&
-			 TryGetValue(key, jsonTypeInfo, out TValue existingReference) &&
+	public bool Upsert<TValue>(ReadOnlySpan<char> key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo, Func<TValue, bool> updateCondition) {
+		if (TryGetValue(key, jsonTypeInfo, out TValue existingReference) &&
 			 !updateCondition(existingReference)) {
 			return false;
 		}
-		WaitIfSerializing(); // block if the database is currently serializing
-		byte[] utf8value = JsonSerializer.SerializeToUtf8Bytes(value, jsonTypeInfo);
-		Lookup[key] = utf8value;
-		OnChangeInternal(ArrowDbChangeEventArgs.Upsert); // trigger change event
-		return true;
+		return Upsert(key, value, jsonTypeInfo);
 	}
 }
