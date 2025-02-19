86f9cc41f83c7401666b36c594d330d00254eaff
David Shnayder <dusrdev@gmail.com>
2025-02-19 17:04:59 +0200

    Added GetOrAddAsync

--- src/ArrowDbCore/ArrowDb.GetOrAdd.cs
+++ src/ArrowDbCore/ArrowDb.GetOrAdd.cs
@@ -10,15 +10,15 @@ public partial class ArrowDb {
 	/// <typeparam name="TValue">The type of the value to get or add</typeparam>
 	/// <param name="key">The key at which to find or add the value</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
-	/// <param name="factory">The function used to generate a value for the key</param>
+	/// <param name="valueFactory">The function used to generate a value for the key</param>
 	/// <returns>The value after finding or adding it</returns>
 	/// <remarks>
 	/// </remarks>
-	public async ValueTask<TValue> GetOrAddAsync<TValue>(string key, JsonTypeInfo<TValue> jsonTypeInfo, Func<string, ValueTask<TValue>> factory) {
+	public async ValueTask<TValue> GetOrAddAsync<TValue>(string key, JsonTypeInfo<TValue> jsonTypeInfo, Func<string, ValueTask<TValue>> valueFactory) {
 		if (Lookup.TryGetValue(key, out var source)) {
 			return JsonSerializer.Deserialize(new ReadOnlySpan<byte>(source), jsonTypeInfo)!;
 		}
-		var val = await factory(key);
+		var val = await valueFactory(key);
 		Upsert(key, val, jsonTypeInfo);
 		return val;
 	}
