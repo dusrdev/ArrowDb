0a0b4d95a9b7144ff400f246f7ef0920d40d3a13
David Shnayder <dusrdev@gmail.com>
2025-02-19 17:05:11 +0200

    Updated tests

---
+++ tests/ArrowDbCore.Tests.Unit/Upserts.Spans.cs
@@ -0,0 +1,56 @@
+﻿namespace ArrowDbCore.Tests.Unit;
+
+public class Upserts_Spans {
+    [Fact]
+    public async Task Upsert_Span_When_Not_Found_Inserts() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        ReadOnlySpan<char> key = "1";
+        db.Upsert(key, 1, JContext.Default.Int32);
+        Assert.True(db.ContainsKey(key));
+        Assert.Equal(1, db.Count);
+    }
+
+    [Fact]
+    public async Task Upsert_Span_When_Found_Overwrites() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        ReadOnlySpan<char> key = "1";
+        db.Upsert(key, 1, JContext.Default.Int32);
+        Assert.True(db.ContainsKey(key));
+        Assert.Equal(1, db.Count);
+        db.Upsert(key, 2, JContext.Default.Int32);
+        Assert.True(db.TryGetValue(key, JContext.Default.Int32, out var value));
+        Assert.Equal(2, value);
+    }
+
+    [Fact]
+    public async Task Conditional_Update_When_Not_Found_Inserts() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        ReadOnlySpan<char> key = "1";
+        Assert.True(db.Upsert(key, 1, JContext.Default.Int32, reference => reference == 3));
+    }
+
+    [Fact]
+    public async Task Conditional_Update_When_Found_And_Valid_Updates() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        ReadOnlySpan<char> key = "1";
+        db.Upsert(key, 1, JContext.Default.Int32);
+        Assert.True(db.Upsert(key, 3, JContext.Default.Int32, reference => reference == 1));
+        Assert.True(db.TryGetValue(key, JContext.Default.Int32, out var value));
+        Assert.Equal(3, value);
+    }
+
+    [Fact]
+    public async Task Conditional_Update_When_Found_And_Invalid_Returns_False() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        ReadOnlySpan<char> key = "1";
+        db.Upsert(key, 1, JContext.Default.Int32);
+        Assert.False(db.Upsert(key, 3, JContext.Default.Int32, reference => reference == 3));
+        Assert.True(db.TryGetValue(key, JContext.Default.Int32, out var value));
+        Assert.Equal(1, value);
+    }
+}
