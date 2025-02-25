6058542b987c806dc3c0ab16ffe7823f21a3101f
David Shnayder <dusrdev@gmail.com>
2025-02-25 19:17:31 +0200

    Updated tests

--- tests/ArrowDbCore.Tests.Unit/GetOrAddAsync.cs
+++ tests/ArrowDbCore.Tests.Unit/GetOrAddAsync.cs
@@ -17,6 +17,22 @@ public class GetOrAddAsync {
         Assert.Equal(1, task.GetAwaiter().GetResult());
 
     }
+
+    [Fact]
+    public async Task GetOrAddAsync_WithTArg_ReturnsSynchronously_WhenExists() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        db.Upsert("1", 1, JContext.Default.Int32); // add before
+        // using a static delegate ensures that closure cannot be allocated
+        var task = db.GetOrAddAsync("1", JContext.Default.Int32, static async (_, value) => {
+            await Task.Delay(1000);
+            return value;
+        }, 1);
+        Assert.True(task.IsCompletedSuccessfully);
+
+        Assert.Equal(1, task.GetAwaiter().GetResult());
+
+    }
 #pragma warning restore xUnit1031 // Do not use blocking task operations in test method
 
     [Fact]
@@ -28,6 +44,20 @@ public class GetOrAddAsync {
             await Task.Delay(1000);
             return 1;
         });
+        Assert.False(task.IsCompletedSuccessfully);
+        Assert.Equal(1, await task);
+    }
+
+    [Fact]
+    public async Task GetOrAddAsync_WithTArg_ReturnsAsynchronously_WhenNotExists() {
+        var db = await ArrowDb.CreateInMemory();
+        Assert.Equal(0, db.Count);
+        // doesn't exist
+        // using a static delegate ensures that closure cannot be allocated
+        var task = db.GetOrAddAsync("1", JContext.Default.Int32, static async (_, value) => {
+            await Task.Delay(1000);
+            return value;
+        }, 1);
         Assert.False(task.IsCompletedSuccessfully);
         Assert.Equal(1, await task);
     }
