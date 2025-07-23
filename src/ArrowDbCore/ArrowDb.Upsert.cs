7c1fe5ed53cb3406e42b5bb5eee73ca91b58ee44
David Shnayder <dusrdev@gmail.com>
2025-07-23 12:26:33 +0300

    Enforced rejection of nulls in upsert

--- src/ArrowDbCore/ArrowDb.Upsert.cs
+++ src/ArrowDbCore/ArrowDb.Upsert.cs
@@ -6,27 +6,27 @@ namespace ArrowDbCore;
 
 public partial class ArrowDb {
 	/// <summary>
-	/// Upsert the specified key with the specified value into the database
+	/// Upsert the specified key with the specified value into the database.
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
 	/// <param name="key">The key at which to upsert the value</param>
-	/// <param name="value">The value to upsert</param>
+	/// <param name="value">The value to upsert. This cannot be null.</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
-	/// <returns>True</returns>
+	/// <returns>True if the value was upserted, false if the provided value was null.</returns>
 	public bool Upsert<TValue>(string key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo) {
 		return UpsertCore<string, TValue, StringAccessor>(key, value, jsonTypeInfo, default);
 	}
 
 	/// <summary>
-	/// Upsert the specified key with the specified value into the database
+	/// Upsert the specified key with the specified value into the database.
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
 	/// <param name="key">The key at which to upsert the value</param>
-	/// <param name="value">The value to upsert</param>
+	/// <param name="value">The value to upsert. This cannot be null.</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
-	/// <returns>True</returns>
+	/// <returns>True if the value was upserted, false if the provided value was null.</returns>
 	/// <remarks>
-	/// This method overload which uses ReadOnlySpan{char} will not allocate a new string for the key if it already exists, instead it will directly replace the value
+	/// This method overload which uses ReadOnlySpan{char} will not allocate a new string for the key if it already exists, instead it will directly replace the value.
 	/// </remarks>
 	public bool Upsert<TValue>(ReadOnlySpan<char> key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo) {
 		return UpsertCore<ReadOnlySpan<char>, TValue, ReadOnlySpanAccessor>(key, value, jsonTypeInfo, default);
@@ -36,6 +36,9 @@ public partial class ArrowDb {
 	private bool UpsertCore<TKey, TValue, TAccessor>(TKey key, TValue value, JsonTypeInfo<TValue> jsonTypeInfo, TAccessor accessor)
 	where TKey : allows ref struct
 	where TAccessor : IDictionaryAccessor<TKey>, allows ref struct {
+		if (value is null) {
+			return false;
+		}
 		WaitIfSerializing(); // Block if serializing
 		byte[] utf8Value = JsonSerializer.SerializeToUtf8Bytes(value, jsonTypeInfo);
 		accessor.Upsert(this, key, utf8Value);
@@ -44,15 +47,18 @@ public partial class ArrowDb {
 	}
 
 	/// <summary>
-	/// Tries to upsert the specified key with the specified value into the database
+	/// Tries to upsert the specified key with the specified value into the database.
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
 	/// <param name="key">The key at which to upsert the value</param>
-	/// <param name="value">The value to upsert</param>
+	/// <param name="value">The value to upsert. This cannot be null.</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
 	/// <param name="updateCondition">A conditional check that determines whether this update should be performed</param>
-	/// <returns>True if the value was upserted, false otherwise</returns>
+	/// <returns>True if the value was upserted, false otherwise.</returns>
 	/// <remarks>
+	/// <para>
+	/// The operation will be rejected if the provided <paramref name="value"/> is null.
+	/// </para>
 	/// <para>
 	/// <paramref name="updateCondition"/> can be used to resolve write conflicts, the update will be rejected only if both conditions are met:
 	/// </para>
@@ -68,17 +74,20 @@ public partial class ArrowDb {
 	}
 
     /// <summary>
-    /// Tries to upsert the specified key with the specified value into the database
+    /// Tries to upsert the specified key with the specified value into the database.
     /// </summary>
     /// <typeparam name="TValue">The type of the value to upsert</typeparam>
     /// <typeparam name="TArg">The type of the argument for the updateCondition function</typeparam>
     /// <param name="key">The key at which to upsert the value</param>
-    /// <param name="value">The value to upsert</param>
+    /// <param name="value">The value to upsert. This cannot be null.</param>
     /// <param name="jsonTypeInfo">The json type info for the value type</param>
     /// <param name="updateCondition">A conditional check that determines whether this update should be performed</param>
     /// <param name="updateConditionArgument">An argument that could be provided to the updateCondition function to avoid a closure</param>
-    /// <returns>True if the value was upserted, false otherwise</returns>
+    /// <returns>True if the value was upserted, false otherwise.</returns>
     /// <remarks>
+    /// <para>
+    /// The operation will be rejected if the provided <paramref name="value"/> is null.
+    /// </para>
     /// <para>
     /// <paramref name="updateCondition"/> can be used to resolve write conflicts, the update will be rejected only if both conditions are met:
     /// </para>
@@ -94,15 +103,18 @@ public partial class ArrowDb {
 	}
 
 	/// <summary>
-	/// Tries to upsert the specified key with the specified value into the database
+	/// Tries to upsert the specified key with the specified value into the database.
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
 	/// <param name="key">The key at which to upsert the value</param>
-	/// <param name="value">The value to upsert</param>
+	/// <param name="value">The value to upsert. This cannot be null.</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
 	/// <param name="updateCondition">A conditional check that determines whether this update should be performed</param>
-	/// <returns>True if the value was upserted, false otherwise</returns>
+	/// <returns>True if the value was upserted, false otherwise.</returns>
 	/// <remarks>
+	/// <para>
+	/// The operation will be rejected if the provided <paramref name="value"/> is null.
+	/// </para>
 	/// <para>
 	/// <paramref name="updateCondition"/> can be used to resolve write conflicts, the update will be rejected only if both conditions are met:
 	/// </para>
@@ -121,17 +133,20 @@ public partial class ArrowDb {
 	}
 
 	/// <summary>
-	/// Tries to upsert the specified key with the specified value into the database
+	/// Tries to upsert the specified key with the specified value into the database.
 	/// </summary>
 	/// <typeparam name="TValue">The type of the value to upsert</typeparam>
 	/// <typeparam name="TArg">The type of the argument for the updateCondition function</typeparam>
 	/// <param name="key">The key at which to upsert the value</param>
-	/// <param name="value">The value to upsert</param>
+	/// <param name="value">The value to upsert. This cannot be null.</param>
 	/// <param name="jsonTypeInfo">The json type info for the value type</param>
 	/// <param name="updateCondition">A conditional check that determines whether this update should be performed</param>
 	/// <param name="updateConditionArgument">An argument that could be provided to the updateCondition function to avoid a closure</param>
-	/// <returns>True if the value was upserted, false otherwise</returns>
+	/// <returns>True if the value was upserted, false otherwise.</returns>
 	/// <remarks>
+	/// <para>
+	/// The operation will be rejected if the provided <paramref name="value"/> is null.
+	/// </para>
 	/// <para>
 	/// <paramref name="updateCondition"/> can be used to resolve write conflicts, the update will be rejected only if both conditions are met:
 	/// </para>
